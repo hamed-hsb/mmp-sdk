58a280932dd4e97da7990a9693afbc0319599878
yahyalmh <yahyamh01@gmail.com>
2021-10-03 11:59:02 +0330

    Add root project and setup core moudle

---
+++ MoTrack/core-sdk/src/main/java/com/motrack/sdk/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.motrack.sdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
